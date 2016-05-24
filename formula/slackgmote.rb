64afd67f7cf6717b9ec8f906299a7dc91907961f
Alex Ives <alex.ives@govdelivery.com>
2016-05-23 19:55:35 -0500

    Added sha.

--- formula/slackgmote.rb
+++ formula/slackgmote.rb
@@ -2,7 +2,7 @@ class Slackgmote < Formula
   desc "Converts gifs to slack emoji"
   homepage "https://github.com/alexives/slackgmote"
   url "https://github.com/alexives/slackgmote/releases/download/v1.0/slackgmote.sh"
-  sha256 ""
+  sha256 "853e9f5d7cb1a7f0f2b1363e7461cd03cad3c481dd0234282a4b1c17e811957c"
   version "1.0"
 
   depends_on 'imagemagick'
