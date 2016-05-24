c2dc3eff69ccb3ad87b71afe0d5722379e01c7a9
Alex Ives <alex.ives@govdelivery.com>
2016-05-23 20:06:33 -0500

    Updated formula to work

--- formula/slackgmote.rb
+++ formula/slackgmote.rb
@@ -1,14 +1,15 @@
 class Slackgmote < Formula
   desc "Converts gifs to slack emoji"
   homepage "https://github.com/alexives/slackgmote"
-  url "https://github.com/alexives/slackgmote/releases/download/v1.0/slackgmote.sh"
+  url "https://github.com/alexives/slackgmote/releases/download/v1.1/slackgmote"
   sha256 "853e9f5d7cb1a7f0f2b1363e7461cd03cad3c481dd0234282a4b1c17e811957c"
   version "1.0"
 
   depends_on 'imagemagick'
 
   def install
-    bin.install_symlink "slackgmote-1.0.sh" => "slackgmote"
+    bin.install Dir["slackgmote"]
   end
 end
+
 
